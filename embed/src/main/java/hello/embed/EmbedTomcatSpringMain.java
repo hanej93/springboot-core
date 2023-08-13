17c9e4feddbacf33f1a68bd391e7820d0167b5de
EJ <hanej93@naver.com>
2023-08-13 23:54:32 +0900

    내장 톰캣3 - 스프링

--- embed/src/main/java/hello/embed/EmbedTomcatSpringMain.java
+++ embed/src/main/java/hello/embed/EmbedTomcatSpringMain.java
@@ -6,20 +6,30 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
-import hello.servlet.HelloServlet;
+import hello.spring.HelloConfig;
 
-public class EmbedTomcatServletMain {
+public class EmbedTomcatSpringMain {
 
 	public static void main(String[] args) throws LifecycleException {
-		System.out.println("EmbedTomcatServletMain.main");
+		System.out.println("EmbedTomcatSpringMain.main");
+
 		//톰캣 설정
 		Tomcat tomcat = new Tomcat();
 		Connector connector = new Connector();
 		connector.setPort(8080);
 		tomcat.setConnector(connector);
 
-		//서블릿 등록
+		// 스프링 컨테이너 생성
+		AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+		appContext.register(HelloConfig.class);
+		
+		// 스프링 MVC 디스패처 서블릿 생성, 스프링 컨테이너 연결
+		DispatcherServlet dispatcher = new DispatcherServlet(appContext);
+		
+		// 디스패처 서블릿 등록
 		Context context = tomcat.addContext("", "/");
 
 		//== 코드 추가 시작==
@@ -31,8 +41,9 @@ public class EmbedTomcatServletMain {
 		docBaseFile.mkdirs();
 		//== 코드 추가 종료==
 
-		tomcat.addServlet("", "helloServlet", new HelloServlet());
-		context.addServletMappingDecoded("/hello-servlet", "helloServlet");
+		tomcat.addServlet("", "dispatcher", dispatcher);
+		context.addServletMappingDecoded("/", "dispatcher");
+
 		tomcat.start();
 	}
 }
